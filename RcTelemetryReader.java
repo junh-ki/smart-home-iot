1ec4dc33f5f67ba3b686b91e390493d2dd6b597e
Junhyung Ki <kijoonh91@gmail.com>
2023-01-29 19:27:59 +0100

    Optimize RcTelemetryReader

--- RcTelemetryReader.java
+++ RcTelemetryReader.java
@@ -1,47 +1,56 @@
-import java.io.RandomAccessFile;
-import java.lang.Exception;
-import java.lang.Thread;
+package com.iot.diagnostic.rc;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.channels.FileChannel;
 
 public class RcTelemetryReader {
 
-    private static final int DATA_BYTE_SIZE_MAX = 7;
     private static final int SAMPLING_FREQUENCY = 1;
     private static final String ABSOLUTE_PATH_DEVICE_ADS_1115 = "/sys/bus/iio/devices/iio:device0/";
     private static final String FILE_NAME_THROTTLE = "in_voltage0-voltage3_raw";
     private static final String FILE_NAME_STEERING = "in_voltage1-voltage3_raw";
     private static final String FILE_NAME_BATTERY = "in_voltage2-voltage3_raw";
-    private static final String READ_MODE = "r";
 
     public static void main(String[] args) {
         final String throttlePath = ABSOLUTE_PATH_DEVICE_ADS_1115 + FILE_NAME_THROTTLE;
         final String steeringPath = ABSOLUTE_PATH_DEVICE_ADS_1115 + FILE_NAME_STEERING;
         final String batteryPath = ABSOLUTE_PATH_DEVICE_ADS_1115 + FILE_NAME_BATTERY;
         try {
-            final RandomAccessFile throttleRAF = new RandomAccessFile(throttlePath, READ_MODE);
-            final RandomAccessFile steeringRAF = new RandomAccessFile(steeringPath, READ_MODE);
-            final RandomAccessFile batteryRAF = new RandomAccessFile(batteryPath, READ_MODE);
+            final FileInputStream throttleFIS = new FileInputStream(throttlePath);
+            final InputStreamReader throttleISR = new InputStreamReader(throttleFIS);
+            final BufferedReader throttleBR = new BufferedReader(throttleISR);
+            final FileChannel throttleFC = throttleFIS.getChannel();
+            final FileInputStream steeringFIS = new FileInputStream(steeringPath);
+            final InputStreamReader steeringISR = new InputStreamReader(steeringFIS);
+            final BufferedReader steeringBR = new BufferedReader(steeringISR);
+            final FileChannel steeringFC = steeringFIS.getChannel();
+            final FileInputStream batteryFIS = new FileInputStream(batteryPath);
+            final InputStreamReader batteryISR = new InputStreamReader(batteryFIS);
+            final BufferedReader batteryBR = new BufferedReader(batteryISR);
+            final FileChannel batteryFC = batteryFIS.getChannel();
             while (true) {
-                System.out.println("-----");
-                throttleRAF.seek(0);
-                steeringRAF.seek(0);
-                batteryRAF.seek(0);
-                final byte[] throttleBytes = new byte[DATA_BYTE_SIZE_MAX];
-                final byte[] steeringBytes = new byte[DATA_BYTE_SIZE_MAX];
-                final byte[] batteryBytes = new byte[DATA_BYTE_SIZE_MAX];
-                throttleRAF.read(throttleBytes);
-                steeringRAF.read(steeringBytes);
-                batteryRAF.read(batteryBytes);
-                final String throttleValue = new String(throttleBytes);
-                final String steeringValue = new String(steeringBytes);
-                final String batteryValue = new String(batteryBytes);
-                System.out.println("throttleValue: " + throttleValue);
-                System.out.println("steeringValue: " + steeringValue);
-                System.out.println("batteryValue: " + batteryValue);
-                //Thread.sleep(SAMPLING_FREQUENCY);
+                System.out.println("------------------");
+                System.out.println("Throttle: " + getMetric(throttleBR, throttleFC));
+                System.out.println("Steering: " + getMetric(steeringBR, steeringFC));
+                System.out.println("Battery: " + getMetric(batteryBR, batteryFC));
+                Thread.sleep(SAMPLING_FREQUENCY);
             }
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
         }
+    }
+
+    private static String getMetric(BufferedReader bufferedReader, FileChannel fileChannel) {
+        try {
+            final String throttleValue = bufferedReader.readLine();
+            fileChannel.position(0);
+            return throttleValue;
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+        return null;
     }
 
 }
